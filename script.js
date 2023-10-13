d5b33d5c4939332b8ecadf2456b5a9aa242629c5
JADundee <Jadundee@icloud.com>
2023-10-13 06:38:50 -0600

    Finished project

--- script.js
+++ script.js
@@ -1,12 +1,47 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+const randomButton = document.getElementById("randomizer");
+
 
 // Initial gradient
 
 window.onload = function() {
 	setGradient()
+}
+
+// Button functionality
+
+const createHex = () => {
+	let hexCode = "";
+	let hexValues = "0123456789abcdef";
+  
+	for (let i = 0; i < 6; i++) {
+	  hexCode += hexValues.charAt(
+		Math.floor(Math.random() * hexValues.length)
+	  );
+	}
+	return hexCode;
+  };
+
+const randomGradient = () => {
+	let randomColor1 = createHex();
+	let randomColor2 = createHex();
+	color1.value = `#${randomColor1}`;
+	color2.value = `#${randomColor2}`;
+
+
+
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+
 }
 
 function setGradient() {
@@ -20,9 +55,12 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+
+
+
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
+randomButton.addEventListener("click", randomGradient);
 
-console.log(color1.value)
